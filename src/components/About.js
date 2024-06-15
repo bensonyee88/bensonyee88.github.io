fcfa57ffa65405c4973334dd831fd249f24bfa08
bensonyee88 <116570778+bensonyee88@users.noreply.github.com>
2024-06-15 15:12:23 -0400

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+  <section id="about">
+    <h2>About Me</h2>
+    <p>Hello! I'm Benson Yee, a Computer Science student at Cornell University</p>
+  </section>
+);
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Contact = () => (
+  <section id="contact">
+    <h2>Contact</h2>
+    <p>Email: <a href="mailto:by88@cornell.edu">by88@cornell.edu</a></p>
+  </section>
+);
+
+export default Contact;
